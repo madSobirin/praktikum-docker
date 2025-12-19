c5783735c65b47cfe4203fd929f29bcf3ffafe9c
madSobirin <gamingbrn202@gmail.com>
2025-12-19 22:31:58 +0700

    menamabhkan sendinblue untuk kirim email

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\ServiceProvider;
-
+use Symfony\Component\Mailer\Transport\Dsn;
+use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,5 +25,12 @@ class AppServiceProvider extends ServiceProvider
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
+        ;
+
+        Mail::extend('brevo', function (array $config) {
+            return (new BrevoTransportFactory)->create(
+                new Dsn('brevo+api', 'default', $config['key'])
+            );
+        });
     }
 }
